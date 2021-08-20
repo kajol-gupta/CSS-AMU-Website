620cbf2c67117ee7809bf1286ece117c5c6d6102
tada-shi <sadaf9430.rumi@gmail.com>
2021-08-20 14:17:11 +0530

    team page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Team from "./OurTeam/Team";
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* <h1>CSS AMU Website Frontend</h1> */}
+      <Team/>
     </div>
   );
 }
